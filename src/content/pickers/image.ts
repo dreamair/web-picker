9a3cb1a573d3252e4035c117348d8ebc998334b9
bruno <bfeurer@gmx.net>
2024-12-15 21:49:52 +0100

    Pick the current source for images. #3

--- src/content/pickers/image.ts
+++ src/content/pickers/image.ts
@@ -2,16 +2,19 @@ import type { Field } from '../../model/Field.js'
 import { pickElement } from './pickElement.js'
 
 export async function pickImage(field: Field) {
-	const element = await pickElement<HTMLImageElement>(elements => {
-		const element = elements.find(e => e.tagName === 'IMG')
-		return element && 'src' in element && element.src ? element : null
-	})
-	const value = element?.src
-	const alt = element?.alt
-	return value === null
-		? { action: 'cancel', payload: field }
-		: {
-			action: 'setField',
-			payload: { ...field, value, alt, source: location.href }
-		}
+  const element = await pickElement<HTMLImageElement>(elements => {
+    const element = elements.find(e => e.tagName === 'IMG')
+    return element && (('src' in element && element.src) ||
+      ('currentSrc' in element && element.currentSrc))
+      ? element
+      : null
+  })
+  const value = element?.currentSrc || element?.src
+  const alt = element?.alt
+  return value === null
+    ? { action: 'cancel', payload: field }
+    : {
+      action: 'setField',
+      payload: { ...field, value, alt, source: location.href }
+    }
 }
